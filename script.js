95cebd8b711816c2636242366dc7c3d489ae4710
Matthew Sichterman <msichterman1@gmail.com>
2019-05-21 16:12:58 -0500

    Added smooth scrolling and auto menu close. Added a CSS Trick

--- script.js
+++ script.js
@@ -9,3 +9,78 @@ document.getElementById("open-button").addEventListener("click", function() {
     navbarContent.className = "navbar-content-off"
   }
 });
+
+$(document).ready(function() {
+  // NAVIGATION --------------------------//
+  // Introduction Link
+  $("nav")
+    .find("#introduction-link")
+    .click(function(e) {
+      e.preventDefault();
+      navbarContent.className = "navbar-content-off";
+      var section = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(section).offset().top
+      });
+    });
+
+  // HTML Tricks Link
+  $("nav")
+    .find("#html-tricks-link")
+    .click(function(e) {
+      e.preventDefault();
+      navbarContent.className = "navbar-content-off";
+      var section = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(section).offset().top
+      });
+    });
+
+  // CSS Tricks Link
+  $("nav")
+    .find("#css-tricks-link")
+    .click(function(e) {
+      e.preventDefault();
+      navbarContent.className = "navbar-content-off";
+      var section = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(section).offset().top
+      });
+    });
+
+  // JavaScript Tricks Link
+  $("nav")
+    .find("#javascript-tricks-link")
+    .click(function(e) {
+      e.preventDefault();
+      navbarContent.className = "navbar-content-off";
+      var section = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(section).offset().top
+      });
+    });
+  
+  // Git Tricks Link
+  $("nav")
+    .find("#git-tricks-link")
+    .click(function(e) {
+      e.preventDefault();
+      navbarContent.className = "navbar-content-off";
+      var section = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(section).offset().top
+      });
+    });
+
+  // Other Projects Link
+  $("nav")
+    .find("#other-projects-link")
+    .click(function(e) {
+      e.preventDefault();
+      navbarContent.className = "navbar-content-off";
+      var section = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(document).height() - $(window).height()
+      });
+    });
+  });
